f6ddc21775d9438d4425bce09b1bcececc4c23c1
GhostMaster69-dev <rathore6375@gmail.com>
2023-07-06 17:05:07 +0000

    ANDROID: Add ksu root solution integration
    - source: https://github.com/tiann/KernelSU.git
    - Credit to backslashxx for refactors of original KSU hooks and other things.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -427,6 +427,12 @@ static void input_handle_event(struct input_dev *dev,
 
 }
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+extern bool ksu_input_hook __read_mostly;
+extern __attribute__((cold)) int ksu_handle_input_handle_event(
+			unsigned int *type, unsigned int *code, int *value);
+#endif
+
 /**
  * input_event() - report new input event
  * @dev: device that generated the event
@@ -448,6 +454,11 @@ void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
 	unsigned long flags;
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+	if (unlikely(ksu_input_hook))
+		ksu_handle_input_handle_event(&type, &code, &value);
+#endif
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
 
--- fs/read_write.c
+++ fs/read_write.c
@@ -586,8 +586,19 @@ ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 	return ret;
 }
 
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+extern bool ksu_vfs_read_hook __read_mostly;
+extern __attribute__((cold)) int ksu_handle_vfs_read(unsigned int fd,
+				char __user **buf_ptr, size_t *count_ptr);
+#endif
+
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+	if (unlikely(ksu_vfs_read_hook))
+		ksu_handle_vfs_read(fd, &buf, &count);
+#endif
 	return ksys_read(fd, buf, count);
 }
 
