e35b402a447c00cbfbc5d6273a9a27b6de4fc79e
GhostMaster69-dev <rathore6375@gmail.com>
2026-07-22 18:33:42 +0530

    Android.mk: Add prebuilt kernel headers support
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter land prada santoni ugg,$(TARGET_DEVICE)),)
+
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/
+KERNEL_HEADERS_INSTALL := $(KERNEL_OUT)/usr
+
+$(KERNEL_OUT):
+	mkdir -p $(KERNEL_OUT)
+
+$(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT)
+	cp -Rf $(LOCAL_PATH)/kernel_headers $(KERNEL_HEADERS_INSTALL)
+
+endif
